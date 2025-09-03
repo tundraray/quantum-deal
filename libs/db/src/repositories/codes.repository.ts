497e52acf453542a3a161664c51ecbc3f70f5f84
Mikhail Andreev <tundraray@outlook.com>
2025-09-03 23:41:48 +0300

    ✨ feat: add promo code lookup method to CodesRepository
    - Implement findByCode method to find unused promo codes
    - Add type-safe query with Drizzle ORM conditions
    - Check code exists and userId is null (not activated)
    - Support promo code activation flow in bot

--- libs/db/src/repositories/codes.repository.ts
+++ libs/db/src/repositories/codes.repository.ts
@@ -1,14 +1,21 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { BaseRepository } from './base.repository';
 import { DRIZZLE_CLIENT, type DrizzleClient } from '../database.provider';
-import { codes } from '../schema/codes';
+import { Code, codes, NewCode } from '../schema/codes';
+import { eq, and, isNull } from 'drizzle-orm';
 
 @Injectable()
-export class CodesRepository extends BaseRepository {
+export class CodesRepository extends BaseRepository<Code, NewCode, number> {
   protected table = codes;
   protected idColumn = codes.id;
 
   constructor(@Inject(DRIZZLE_CLIENT) db: DrizzleClient) {
     super(db);
+  }
+
+  public async findByCode(code: string) {
+    const condition = and(eq(this.table.code, code), isNull(this.table.userId));
+
+    return this.findOneBy(condition!);
   }
 }
